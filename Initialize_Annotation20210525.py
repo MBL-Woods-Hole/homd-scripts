12c584908b0fccbbb7f90f2d7de7664e97cd3013
Andrew Voorhis <avoorhis@mbl.edu>
2021-05-25 12:21:54 -0400

    anno from 51

--- Initialize_Annotation20210525.py
+++ Initialize_Annotation20210525.py
@@ -15,7 +15,7 @@ today = str(date.today())
 from connect import MyConnection
 usable_annotations = ['ncbi','prokka']
 # obj = {seqid:[]}             
-def make_anno_object():
+def make_object(args):
 
     new_obj={}
     new_obj['gid'] = ''
@@ -29,64 +29,46 @@ def make_anno_object():
     ## ignore for now repeat_region, misc_RNA
     return new_obj
 
-def find_databases(args):
-    
-    
-    dbs = {}
-    dbs['ncbi'] = []
-    dbs['prokka'] = []
-    for db in dbs:
-        q = "SHOW DATABASES LIKE '"+db+"\_%'"
-        #print(q)
-        result = myconn.execute_fetch_select(q)
-        
-        for row in result:
-            dbs[db].append(row[0])
-    return dbs
-    
-# def fix_typo(dbs):
-# 	""" RISKY Don't Change the db"""
-# 	for db in dbs['ncbi']:
-# 	    print(db)  
-# 	    
-# 	    q = "ALTER TABLE "+db+".`assembly_report' CHANGE `filed_value` `field_value` TEXT"  
-	    
-def run(args,dbs):
+def run(args):
+    """ date not used"""
     #global master_lookup
     master_lookup = {}
-    # prokka first
-    for db in dbs['prokka']:
-        
-        gid = db.split('_')[1]
-        anno = 'prokka'
-        
-        
+    qinit = "SELECT distinct gid from annotation.genome"
+    base_result = myconn.execute_fetch_select(qinit)
+    for row in base_result:
+        gid = row[0]
         if gid not in master_lookup:
             master_lookup[gid] = {}
-            master_lookup[gid]['prokka'] = make_anno_object()
-            master_lookup[gid]['prokka']['gid'] = gid
-            master_lookup[gid]['ncbi']   = make_anno_object()
-            master_lookup[gid]['ncbi']['gid'] = gid
-        
-        q = "SELECT * from "+db+".prokka"
-        #print(q)
-        result = myconn.execute_fetch_select_dict(q)
-        for row in result:
-            for n in row:
-                if n in master_lookup[gid]['prokka']:
-                    master_lookup[gid]['prokka'][n] = str(row[n]).strip()
-                        
+        master_lookup[gid]['prokka'] = make_object(args)
+        master_lookup[gid]['ncbi']   = make_object(args)
     
-    for db in dbs['ncbi']:
-        gid = db.split('_')[1]
-        anno = 'ncbi'
+    
+    #print(master_lookup)
+    
+    # PROKKA FIRST
+    anno = 'prokka'    
+    q = "SELECT * from annotation.prokka_info"
+    result = myconn.execute_fetch_select_dict(q)
+    for row in result:
+        gid = row['gid']
+        for n in row:
+            if n in master_lookup[gid]['prokka']:
+                master_lookup[gid]['prokka'][n] = row[n]
+    
+    ## NEXT NCBI
+    anno = 'ncbi' 
+    for row in base_result:
+        gid = row[0]
+        #print(gid)
         # organism
-        q1 = "select `field_value` from "+db+".assembly_stats WHERE field_name = 'Organism name'"
+        q1 = "select organism from annotation.ncbi_info where gid ='"+gid+"'"
         resultq1 = myconn.execute_fetch_one(q1)
-        organism = resultq1[0].strip()
+        organism = resultq1[0]
         master_lookup[gid]['ncbi']['organism'] = organism
+        master_lookup[gid]['ncbi']['gid'] = gid
+        where_clause = "WHERE gid='"+gid+"' AND annotation='ncbi'"
         # for CDS,rrna,trna,tmrna
-        q2 = "SELECT `type`,count(*) AS count from "+db+".gff group by `type`"
+        q2 = "SELECT `type`,count(*) AS count from annotation.gff "+where_clause+" group by `type`"
         #print(q2)
         result2 = myconn.execute_fetch_select_dict(q2)
         for row in result2:
@@ -98,55 +80,19 @@ def run(args,dbs):
             if row['type'] == 'tmRNA':
                master_lookup[gid]['ncbi']['tmRNA'] = row['count']
             if row['type'] == 'tRNA':
-               master_lookup[gid]['ncbi']['tRNA'] = row['count']     
-            
-        q3 = "SELECT sum(bps) AS bases, count(*) AS contigs FROM "+db+".molecules "
+               master_lookup[gid]['ncbi']['tRNA'] = row['count']  
+         
+        q3 = "SELECT sum(bps) AS bases, count(*) AS contigs FROM annotation.molecule "+where_clause
         result3 = myconn.execute_fetch_select_dict(q3)
         for row in result3:
             #print(row)
-            master_lookup[gid]['ncbi']['bases']   = int(row['bases'])
-            master_lookup[gid]['ncbi']['contigs'] = int(row['contigs'])
-            
-    #print(master_lookup)
-#     
-    ## NEXT NCBI
-#     anno = 'ncbi' 
-#     for row in base_result:
-#         gid = row[0]
-#         #print(gid)
-#         # organism
-#         q1 = "select organism from annotation.ncbi_info where gid ='"+gid+"'"
-#         resultq1 = myconn.execute_fetch_one(q1)
-#         organism = resultq1[0]
-#         master_lookup[gid]['ncbi']['organism'] = organism
-#         master_lookup[gid]['ncbi']['gid'] = gid
-#         where_clause = "WHERE gid='"+gid+"' AND annotation='ncbi'"
-#         # for CDS,rrna,trna,tmrna
-#         q2 = "SELECT `type`,count(*) AS count from annotation.gff "+where_clause+" group by `type`"
-#         #print(q2)
-#         result2 = myconn.execute_fetch_select_dict(q2)
-#         for row in result2:
-#             #print(row)
-#             if row['type'] == 'CDS':
-#                master_lookup[gid]['ncbi']['CDS'] = row['count']
-#             if row['type'] == 'rRNA':
-#                master_lookup[gid]['ncbi']['rRNA'] = row['count']
-#             if row['type'] == 'tmRNA':
-#                master_lookup[gid]['ncbi']['tmRNA'] = row['count']
-#             if row['type'] == 'tRNA':
-#                master_lookup[gid]['ncbi']['tRNA'] = row['count']  
-#          
-#         q3 = "SELECT sum(bps) AS bases, count(*) AS contigs FROM annotation.molecule "+where_clause
-#         result3 = myconn.execute_fetch_select_dict(q3)
-#         for row in result3:
-#             #print(row)
-#             master_lookup[gid]['ncbi']['bases']=int(row['bases'])
-#             master_lookup[gid]['ncbi']['contigs']=int(row['contigs'])
-#         
-#         print(master_lookup[gid]['ncbi'])
-#     
-#     
-#     
+            master_lookup[gid]['ncbi']['bases']=int(row['bases'])
+            master_lookup[gid]['ncbi']['contigs']=int(row['contigs'])
+        
+        print(master_lookup[gid]['ncbi'])
+    
+    
+    
     file =  os.path.join(args.outdir,args.outfileprefix+'Lookup.json')  
     print_dict(file, master_lookup) 
 
@@ -198,29 +144,24 @@ if __name__ == "__main__":
         print("\nThe out put directory doesn't exist:: using the current dir instead\n")
         args.outdir = './'                         
     if args.dbhost == 'homd':
-        #args.DATABASE  = 'homd'
-        dbhost_old = '192.168.1.51'
-        #dbhost_new = '192.168.1.40'
+        args.DATABASE  = 'homd'
+        dbhost = '192.168.1.40'
         args.outdir = '../homd-startup-data/'
         args.prettyprint = False
         
     elif args.dbhost == 'localhost':  #default
-        #args.DATABASE = 'homd'
-        dbhost_old = 'localhost'
+        args.DATABASE = 'homd'
+        dbhost = 'localhost'
     else:
         sys.exit('dbhost - error')
     args.indent = None
     if args.prettyprint:
         args.indent = 4
     print()
-    myconn = MyConnection(host=dbhost_old,   read_default_file = "~/.my.cnf_node")
+    myconn = MyConnection(host=dbhost,   read_default_file = "~/.my.cnf_node")
 
     print(args)
     
-    databases = find_databases(args)
-    
-    #print('dbs',databases)
-    
-    run(args,databases)
+    run(args)
     
     
