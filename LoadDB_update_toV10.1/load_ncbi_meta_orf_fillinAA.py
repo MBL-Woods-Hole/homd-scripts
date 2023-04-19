6fbfc4ddbc983b11cce44daf6db1c8bbcd014e70
avoorhis <avoorhis@mbl.edu>
2023-04-19 13:41:22 -0400

    preserve load

---
+++ LoadDB_update_toV10.1/load_ncbi_meta_orf_fillinAA.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+## SEE https://docs.dhtmlx.com/suite/tree__api__refs__tree.html // new version 7 must load from json file
+# this script creates a list of json objects that allows the dhtmlx javascript library
+# to parse and show a taxonomy tree (Written for HOMD)
+##
+import os, sys
+import gzip
+import json
+#from json import JSONEncoder
+import argparse
+import csv,re
+from Bio import SeqIO
+sys.path.append('../homd-data/')
+sys.path.append('../../homd-data/')
+from connect import MyConnection,mysql
+import datetime
+global mysql_errors
+mysql_errors = []
+"""
+
+"""
+today = str(datetime.date.today())
+
+# should add these: 
+# Missing: isolate origin, Sequencing status??, Combined length, GC percentage, ATCC stuff, num_contigs
+
+
+
+
+def run(args):
+    #for root, dirs, files in os.walk(args.indir):
+    q = "select UNCOMPRESS(seq_compressed) as seq, protein_id from `NCBI_faa`.`protein_seq`"
+    result = myconn.execute_fetch_select(q)
+    for row in result:
+        #print(row)
+        seq = str(row[0],'utf-8')
+        length = len(seq)
+        pid = row[1]
+        #print(length,pid,seq)
+        q2 = "UPDATE `NCBI_meta`.`orf` set length_aa='%s' WHERE protein_id='%s'" % (length, pid)
+        print(q2)
+        res = myconn.execute_no_fetch(q2)
+        print('UPDATE Result:',res)
+        
+        
+
+        
+if __name__ == "__main__":
+
+    usage = """
+    USAGE:
+        ./add_genomes_to_NCBI_METAV10_1.py 
+        
+        host and annotation will determine directory to search
+        
+        -host/--host [homd]  default:localhost
+       
+        print('None Selected: --info, --molecules,  --orf) 
+        
+
+    """
+
+    parser = argparse.ArgumentParser(description="." ,usage=usage)
+
+    #parser.add_argument("-i", "--infile",   required=True,  action="store",   dest = "infile", default='none',
+    #                                                help=" ")
+    
+    parser.add_argument("-v", "--verbose",   required=False,  action="store_true",   dest = "verbose", default=False,
+                                                    help=" ")
+    parser.add_argument("-host", "--host",
+                        required = False, action = 'store', dest = "dbhost", default = 'localhost',
+                        help = "choices=['homd',  'localhost']")
+    
+    parser.add_argument("-s", "--start_digit",   required=False,  action="store",   dest = "start_digit", default='1',
+                                                    help=" ")
+    
+    args = parser.parse_args()
+    
+    #parser.print_help(usage)
+                        
+    if args.dbhost == 'homd':
+        #args.json_file_path = '/groups/vampsweb/vamps/nodejs/json'
+        #args.TAX_DATABASE = 'HOMD_taxonomy'
+        args.DATABASE = 'homd'
+        #dbhost_old = '192.168.1.51'
+        dbhost= '192.168.1.42'   #TESTING is 1.42  PRODUCTION is 1.40
+        args.prettyprint = False
+        #args.ncbi_dir = '/mnt/efs/bioinfo/projects/homd_add_genomes_V10.1/GCA_V10.1_all'
+        #args.prokka_dir = '/mnt/efs/bioinfo/projects/homd_add_genomes_V10.1/prokka_V10.1_all'
+        if args.anno == 'prokka':
+            #args.indir = '/mnt/efs/bioinfo/projects/homd_add_genomes_V10.1/prokka_V10.1_all'
+            args.indir = '/mnt/efs/bioinfo/projects/homd_add_genomes_V10.1_all/add_prokka/prokka'
+        else:
+            #args.indir = '/mnt/efs/bioinfo/projects/homd_add_genomes_V10.1/GCA_V10.1_all'
+            args.indir = '/mnt/efs/bioinfo/projects/homd_add_genomes_V10.1_all/add_ncbi/GCA_V10.1_all'
+    elif args.dbhost == 'localhost':
+        #args.json_file_path = '/Users/avoorhis/programming/homd-data/json'
+        #args.TAX_DATABASE  = 'HOMD_taxonomy'
+        args.DATABASE = 'homd'
+        dbhost = 'localhost'
+        #dbhost_old = 'localhost'
+        
+        
+    else:
+        sys.exit('dbhost - error')
+    
+    
+    myconn = MyConnection(host=dbhost,   read_default_file = "~/.my.cnf_node")
+    #myconn_new = MyConnection(host=dbhost_new, db=args.NEW_DATABASE,  read_default_file = "~/.my.cnf_node")
+#     if args.source not in ['segata','dewhirst','eren']:
+#         sys.exit('no valid source')
+#     if args.source.lower() not in args.infile.lower():
+#         sys.exit('file/source mismatch')
+   
+    
+    run(args)
+    
+    
+    
+    
