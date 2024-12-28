f684f922cafcc492db168b97143e397617cca151
avoorhis <avoorhis@mbl.edu>
2024-12-28 09:45:41 -0500

    ips

--- ip_log_reader.py
+++ ip_log_reader.py
@@ -102,7 +102,7 @@ def format_report2(info,save_list):
     
 def process_line(fxn, dpattern, dformat, l):
     matches = re.findall(dpattern, l)
-    print('matches',matches,l)
+    print('matches',dpattern,matches,l)
     if len(matches) == 0:
         
         sys.exit('date match error')
